80dd7ec9f2639677db8f2eff689aac159bf4469f
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-04-01 22:53:07 +0200

    ajout des DTOs

---
+++ src/EnchereService/DTOs/CreateEnchereDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnchereService;
+
+public class CreateEnchereDto
+{
+    [Required]
+    public string Make { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public int Year { get; set; }
+    [Required]
+    public string Color { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string ImageUrl { get; set; }
+    [Required]
+    public string Category { get; set; }
+    [Required]
+    public int ReservePrice { get; set; }
+    [Required]
+    public string State { get; set; }
+    [Required]
+    public DateTime AuctionEnd { get; set; }
+}
---
+++ src/EnchereService/DTOs/UpdateEnchereDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EnchereService;
+
+public class UpdateEnchereDto
+{
+    public string Make { get; set; }
+    public string Name { get; set; }
+    public int? Year { get; set; }
+    public string Color { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string Category { get; set; }
+    public string State { get; set; }
+}
