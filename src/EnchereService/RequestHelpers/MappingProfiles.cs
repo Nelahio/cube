30f3283313ed3ed87806184385fed004d560c8b9
Amandine <nelasura@gmail.com>
2024-04-02 19:59:58 +0200

    configuration d'automapper

---
+++ src/EnchereService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EnchereService.DTOs;
+using EnchereService.Models;
+
+namespace EnchereService;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Enchere, EnchereDto>().IncludeMembers(e => e.Item);
+        CreateMap<Produit, EnchereDto>();
+        CreateMap<CreateEnchereDto, Enchere>()
+        .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateEnchereDto, Produit>();
+    }
+}
