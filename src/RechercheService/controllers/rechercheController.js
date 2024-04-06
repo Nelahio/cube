4c914e5a5c0a923b42a12276011c6ddecc864bca
Amandine <nelasura@gmail.com>
2024-04-06 12:20:05 +0200

    ajout e la pagination service recherche

--- src/RechercheService/controllers/rechercheController.js
+++ src/RechercheService/controllers/rechercheController.js
@@ -3,6 +3,9 @@ const Produit = require('../models/produit');
 exports.rechercheProduits = async (req, res) => {
     try {
         const searchTerm = req.query.searchTerm;
+        const pageNumber = parseInt(req.query.pageNumber, 10) || 1;
+        const pageSize = parseInt(req.query.pageSize, 10) || 4;
+
         let query = Produit.find();
 
         if (searchTerm) {
@@ -13,8 +16,21 @@ exports.rechercheProduits = async (req, res) => {
             query = query.sort('Make');
         }
 
-        const result = await query.exec();
-        res.json(result);
+        // Calcul pour la pagination
+        const skip = (pageNumber - 1) * pageSize;
+        query = query.skip(skip).limit(pageSize);
+
+        // Exécution de la requête avec pagination
+        const results = await query.exec();
+
+        // Calcul du nombre total de documents pour déterminer le nombre de pages
+        const totalCount = await Produit.countDocuments(searchTerm ? { $text: { $search: searchTerm } } : {});
+
+        res.json({
+            results,
+            pageCount: Math.ceil(totalCount / pageSize),
+            totalCount
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
