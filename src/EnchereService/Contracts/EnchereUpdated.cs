dbfff287c646a0b4db11c72861ea57e54df814b2
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-04-08 13:32:36 +0200

    ajout du contrat

---
+++ src/EnchereService/Contracts/EnchereUpdated.cs
@@ -0,0 +1,14 @@
+﻿namespace EnchereService.Contracts;
+
+public class EnchereUpdated
+{
+    public string Id { get; set; }
+    public string Make { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Category { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string State { get; set; }
+}
