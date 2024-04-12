506ad0d206a77774e71811ba18ba3ec5cae13109
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-04-12 09:39:41 +0200

    ajout custom profile service authentification

--- src/AuthentificationService/HostingExtensions.cs
+++ src/AuthentificationService/HostingExtensions.cs
@@ -4,6 +4,7 @@ using AuthentificationService.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using AuthentificationService.Services;
 
 namespace AuthentificationService;
 
@@ -34,7 +35,8 @@ internal static class HostingExtensions
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients)
-            .AddAspNetIdentity<ApplicationUser>();
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddProfileService<CustomProfileService>();
 
         builder.Services.ConfigureApplicationCookie(options =>
         {
