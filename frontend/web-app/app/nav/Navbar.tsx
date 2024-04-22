a95a075eb1277a34520548ebaa9d10cac0f512d3
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-04-22 23:55:54 +0200

    création de la navbar frontend

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { GiDelicatePerfume } from "react-icons/gi";
+
+export default function Navbar() {
+  return (
+    <header
+      className="
+        sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
+        "
+    >
+      <div className="flex items-center gap-2 text-3xl font-semibold text-purple-700 shadow-md tracking-tight hover:text-yellow-600 transition-colors">
+        <GiDelicatePerfume size={40} />
+        <div>Cube Enchères</div>
+      </div>
+      <div>Rechercher</div>
+      <div>Login</div>
+    </header>
+  );
+}
