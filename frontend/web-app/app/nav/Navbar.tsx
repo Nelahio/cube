9117ef6467e4dd83e4a8e3a64aaffdd99fa32c76
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-04-28 04:54:46 +0200

    refacto logo + barre de recherche fonctionnelle frontend

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { GiDelicatePerfume } from "react-icons/gi";
 import Recherche from "./Recherche";
+import Logo from "./Logo";
 
 export default function Navbar() {
   return (
     <header className="header sticky top-0 z-50 flex justify-between bg-white p-5 items-center shadow-md">
-      <div className="flex items-center gap-2 text-3xl font-semibold text-purple-700 tracking-tight hover:text-yellow-400 transition-colors">
-        <GiDelicatePerfume size={40} />
-        <div>Cube Enchères</div>
-      </div>
+      <Logo />
       <Recherche />
       <button className="login-btn hover:bg-gray-100 hover:text-gray-600 transition duration-200 ease-in-out px-4 py-2 rounded-md text-base font-medium">
         Connexion
--- frontend/web-app/app/nav/Recherche.tsx
+++ frontend/web-app/app/nav/Recherche.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+"use client";
+
+import { useParamsStore } from "@/hooks/useParamsStore";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function Recherche() {
+  const setParams = useParamsStore((state) => state.setParams);
+  const [value, setValue] = useState("");
+
+  function onChange(event: any) {
+    setValue(event.target.value);
+  }
+
+  function search() {
+    setParams({ searchTerm: value });
+  }
+
   return (
     <div className="flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm">
       <input
+        onKeyDown={(e: any) => {
+          if (e.key === "Enter") search();
+        }}
+        onChange={onChange}
         type="text"
         placeholder="Rechercher une enchère par sa marque, son modèle ou sa couleur"
         className="flex-grow pl-5 bg-transparent focus:outline-none border-transparent focus:border-transparent focus:ring-0 text-sm text-gray-600"
       />
-      <button>
+      <button onClick={search}>
         <FaSearch
           size={34}
           className="bg-purple-600 text-white rounded-full p-2 cursor-pointer mx-2"
