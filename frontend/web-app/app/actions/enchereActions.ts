440ee15585d4df1afa343be4b766c0cebd03b571
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-04-29 17:18:17 +0200

    ajout test authentification frontend

--- frontend/web-app/app/actions/enchereActions.ts
+++ frontend/web-app/app/actions/enchereActions.ts
@@ -9,3 +9,22 @@ export async function getData(query: string): Promise<PagedResult<Enchere>> {
 
   return res.json();
 }
+
+export async function UpdateEnchereTest() {
+  const data = {
+    size: Math.floor(Math.random() * 10000) + 1,
+  };
+
+  const res = await fetch(
+    "http://localhost:6001/encheres/6a5011a1-fe1f-47df-9a32-b5346b289391",
+    {
+      method: "PUT",
+      headers: {},
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!res.ok) return { status: res.status, message: res.statusText };
+
+  return res.statusText;
+}
--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "../actions/authActions";
 import React from "react";
 import Heading from "../components/Heading";
+import AuthTest from "./AuthTest";
 
 export default async function Session() {
   const session = await getSession();
@@ -10,6 +11,9 @@ export default async function Session() {
       <div className="bg-blue-200 border-2 border-blue-500">
         <h3 className="text-lg">Données de session</h3>
         <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+      <div className="mt-4">
+        <AuthTest />
       </div>
     </div>
   );
