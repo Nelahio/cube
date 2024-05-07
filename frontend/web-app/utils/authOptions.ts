3b3c345e1e01602ec4fef2b6246cb1ec0128b000
Amandine <nelasura@gmail.com>
2024-05-07 03:28:46 +0200

    corrections des bugs de connexion de la web-app au service authentification

--- frontend/web-app/utils/authOptions.ts
+++ frontend/web-app/utils/authOptions.ts
@@ -10,7 +10,7 @@ export const authOptions: NextAuthOptions = {
       id: "id-server",
       clientId: "nextApp",
       clientSecret: "secret",
-      issuer: "http://localhost:5000",
+      issuer: process.env.AUTH_URL,
       authorization: { params: { scope: "openid profile cubeApp" } },
       idToken: true,
     }),
